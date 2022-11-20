7d4b448eabce13940934a7947e45f5ecfcdad70e
VitalikApter <aptvetal2014@gmail.com>
2022-11-20 17:41:09 +0200

    3
    сделал задания,1-3
    не работает живая страница

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Vimeo from '@vimeo/player';
 
+import throttle from 'lodash.throttle';
+
+
+const onPlay = ({seconds}) => {
+    localStorage.setItem('videoplayer-current-time', seconds);
+};
+
+const vimeoPlayerEl = document.querySelector('#vimeo-player');
+
+const player = new Vimeo(vimeoPlayerEl);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
